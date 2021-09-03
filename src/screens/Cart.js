ad9e3576571a0ee973f7cc5a3ed36039010193b4
Resindu <resindu.tharuprocessing@gmail.com>
2021-09-03 19:58:31 +0530

    cart

---
+++ src/screens/Cart.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text ,SafeAreaView,StyleSheet} from 'react-native';
+function Cart() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Cart Screen</Text>
+      </View>
+    );
+  }
+  export default Cart 
